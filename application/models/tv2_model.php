1fb0c6d38beb27902260736e8feb5168f7f711d1
Nattapong Tonprasert <luciferultram@gmail.com>
2013-12-04 00:26:16 +0700

    wp and s40 show channel only has_show

--- application/models/tv2_model.php
+++ application/models/tv2_model.php
@@ -117,8 +117,13 @@ class Tv2_model extends CI_Model
 		{
 			$sql = "SELECT id, title, description, CASE thumbnail WHEN '' THEN '' ELSE CONCAT('$this->channel_thumbnail_path', thumbnail) END AS thumbnail, url_$this->device url 
 			FROM tv_channel
-			WHERE online = 1 
-			ORDER BY `order`, title";
+			WHERE online = 1 ";
+			if ($this->device == "wp" || $this->device == "s40")
+			{
+				$sql .= " AND has_show = 1 ";
+			}
+			$sql .= " ORDER BY `order`, title";
+
 			return $this->db->query($sql)->result();
 		}
 		else
