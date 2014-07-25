ffe7142ccda7b5e7f5f4a844d3847c8fcecacfe6
Nattapong Tonprasert <luciferultram@gmail.com>
2014-07-25 18:15:18 +0700

    add controller privacy

---
+++ application/views/privacy.php
@@ -0,0 +1,34 @@
+<?php
+$this->load->helper('html');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>TV Thailand Privacy Policy</title>
+
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22403997-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+<body>
+
+<h2>Third Party Advertisers</h2>
+
+<hr />
+
+We permit advertisements to be delivered to you by third party Internet advertising companies (also called ad networks or network advertisers). These companies may use cookies, Web beacons, platform device identifiers, software agents, and other technologies to collect non-personally identifiable information about your visits over time on our Service and across other websites to deliver advertisements to you targeted to your interests, measure their effectiveness and personalize advertising content, and to understand the usage and visitation of the Services and the other applications and websites tracked by these advertising companies. We do not have access to or control over cookies, Web beacons, platform device identifiers, software agents, or other technologies that they may use. We are not responsible for the privacy practices of third party advertisers. You should check the privacy policy of the third party advertiser to determine how it handles information it separately collects from you. In addition, the <a href="http://www.networkadvertising.org/" rel="nofollow">Network Advertising Initiative</a> offers information about some of the Internet advertising companies we may use. To learn about Apple’s interest based advertising visit <a href="http://www.apple.com/privacy/" rel="nofollow">Apple’s privacy policy</a>.
+
+</body>
+</html>
